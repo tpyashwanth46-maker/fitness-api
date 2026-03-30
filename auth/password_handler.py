513087b81cc62257a40c68b4d345207af8eeb854
Yashwanth <tpyashwanth46@gmail.com>
2026-03-30 16:28:22 +0530

    fix package structure

--- auth/password_handler.py
+++ auth/password_handler.py
@@ -1,12 +1,10 @@
-from passlib.context import CryptContext
 import hashlib
+from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    # First hash using SHA256 (no limit)
     hashed = hashlib.sha256(password.encode()).hexdigest()
-    # Then apply bcrypt
     return pwd_context.hash(hashed)
 
 def verify_password(plain_password: str, hashed_password: str):
