16e0d9f83a0125f57a579fe59b9dc888caa3709a
Yashwanth <tpyashwanth46@gmail.com>
2026-03-30 15:02:25 +0530

    fix bcrypt length issue

--- auth/password_handler.py
+++ auth/password_handler.py
@@ -4,8 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
