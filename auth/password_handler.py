93af14abfa5acfe2a0c0ea8029de69a3c5eafa93
Yashwanth <tpyashwanth46@gmail.com>
2026-03-30 15:17:03 +0530

    added sha256 before bcrypt

--- auth/password_handler.py
+++ auth/password_handler.py
@@ -1,11 +1,14 @@
 from passlib.context import CryptContext
+import hashlib
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])
-
+    # First hash using SHA256 (no limit)
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    # Then apply bcrypt
+    return pwd_context.hash(hashed)
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password[:72], hashed_password)
+    hashed = hashlib.sha256(plain_password.encode()).hexdigest()
+    return pwd_context.verify(hashed, hashed_password)
