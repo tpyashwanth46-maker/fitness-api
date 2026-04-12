a3b1305fbdce9d58a7312d3a877c2746d54b581c
Yashwanth <tpyashwanth46@gmail.com>
2026-04-12 13:58:28 +0530

    Updated db.py for Render PostgreSQL connection

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:yash@localhost:5432/fitness_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
